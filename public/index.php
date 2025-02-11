787cb21c900fb36e913befe196f1fbb5fb407eb2
Mohammed Ali Boutaine <111361086+mohammed-ali-boutaine@users.noreply.github.com>
2025-02-11 21:58:28 +0100

    finsh auth midle ware and update Router.php to handle middlewares

--- public/index.php
+++ public/index.php
@@ -1,14 +1,20 @@
 <?php 
 
+session_start();
 // require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap.php';
 
+use App\middleware\AuthMiddleware;
 use Core\Router;
+
 Router::get('', 'Home@index');
 Router::get('login', 'AuthController@login');
 Router::get('register', 'AuthController@register');
+
+
+Router::get('dashboard', 'DashboardController@index', [AuthMiddleware::class]);
 // Router::post('', 'AuthController@login');
-Router::dispatch();
+Router::dispatch($_SERVER['REQUEST_URI']);
 
 
 
