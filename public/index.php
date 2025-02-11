f072371031a7c3e6acbfad7fb7e05e7b26a2e5f9
anass17 <anassboutaib2018@gmail.com>
2025-02-11 15:36:15 +0100

    Added Login and Register views

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@ require_once __DIR__ . '/../bootstrap.php';
 
 use Core\Router;
 Router::get('', 'Home@index');
+Router::get('login', 'AuthController@login');
+Router::get('register', 'AuthController@register');
+// Router::post('', 'AuthController@login');
 Router::dispatch();
 
 
