d54b8de91378ca1d4f2ab67711ed3497ef4ac91d
anass17 <anassboutaib2018@gmail.com>
2025-02-12 15:21:48 +0100

    Added file

--- app/models/admin.php
+++ app/models/admin.php
@@ -1,2 +1,21 @@
-<?php 
+<?php
 
+
+namespace APP\Models;
+
+class Admin extends User
+{
+
+     protected static string $table = 'admins'; // Define table name
+
+     function __construct($id, $username, $email, $password)
+     {
+
+        parent::__construct($id, $username, $email, $password);
+
+        $this -> role = "admin";
+
+     }
+
+    
+}
